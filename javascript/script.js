7d543835ee7a3a9ef37084d327b5952a9d14b080
miurakouta <mk.craft.website@gmail.com>
2024-09-28 19:36:12 +0900

    9月28日19時修正

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,20 @@
 //スムーススクロールはindex.htmlの一番下に記述
+
+// ハンバーガ―メニュー
+const hamburgerNav = document.querySelector(".bl_hamburger_nav");
+const hamburgerBox = document.querySelector(".bl_hamburger_box");
+const hamburgerMenu = document.querySelector(".bl_hamburger_menu");
+const hamburgerItem = document.querySelector(".bl_hamburger_item");
+
+hamburgerBox.addEventListener("click", function () {
+    hamburgerNav.classList.toggle("active");
+    hamburgerBox.classList.toggle("active");
+    hamburgerMenu.classList.toggle("active");
+});
+
+hamburgerItem.addEventListener("click", function () {
+    hamburgerNav.classList.toggle("active");
+    hamburgerBox.classList.toggle("active");
+    hamburgerMenu.classList.toggle("active");
+});
+
