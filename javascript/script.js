ab166af62e37535eb3e3cd5993c26dc59cc66e97
miurakouta <mk.craft.website@gmail.com>
2024-09-29 13:06:08 +0900

    script.jsのハンバーガーメニュー修正

--- javascript/script.js
+++ javascript/script.js
@@ -4,17 +4,19 @@
 const hamburgerNav = document.querySelector(".bl_hamburger_nav");
 const hamburgerBox = document.querySelector(".bl_hamburger_box");
 const hamburgerMenu = document.querySelector(".bl_hamburger_menu");
-const hamburgerItem = document.querySelector(".bl_hamburger_item");
+const hamburgerItem = document.querySelectorAll(".bl_hamburger_item");
 // ↑.bl_hamburger_itemをquerySelectorで取得しているので、ページ内の最初の要素の一つしか取得できていません。querySelectorAllで全ての要素を取得してfor文などでループで回して処理する必要があります。
 
 hamburgerBox.addEventListener("click", function () {
-    hamburgerNav.classList.toggle("active");
-    hamburgerBox.classList.toggle("active");
-    hamburgerMenu.classList.toggle("active");
+  hamburgerNav.classList.toggle("active");
+  hamburgerBox.classList.toggle("active");
+  hamburgerMenu.classList.toggle("active");
 });
 
-hamburgerItem.addEventListener("click", function () {
+for (let i = 0; i < hamburgerItem.length; i++) {
+  hamburgerItem[i].addEventListener("click", function () {
     hamburgerNav.classList.toggle("active");
     hamburgerBox.classList.toggle("active");
     hamburgerMenu.classList.toggle("active");
-});
+  });
+}
